42b22c6357a98c47ac13325d5ce91d4bfb1df871
Yoav <yoav.benzvi@gmail.com>
2020-05-15 12:38:34 +0200

    Updates

---
+++ precache-manifest.10bdfc7625325355f35b7e57866fc526.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e22a19b79fd49d561d72f160fd2975c2",
+    "url": "/youtube-video-player/index.html"
+  },
+  {
+    "revision": "6f074da90ab9a8753d3f",
+    "url": "/youtube-video-player/static/css/main.8fa463b4.chunk.css"
+  },
+  {
+    "revision": "37d54a0ccd966f5d384b",
+    "url": "/youtube-video-player/static/js/2.acc1704d.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/youtube-video-player/static/js/2.acc1704d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f074da90ab9a8753d3f",
+    "url": "/youtube-video-player/static/js/main.b1aa078c.chunk.js"
+  },
+  {
+    "revision": "26ede057e37e2071ce96",
+    "url": "/youtube-video-player/static/js/runtime-main.560b8334.js"
+  }
+]);
